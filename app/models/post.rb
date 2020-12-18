3905a97f0894d2bc139f21ee99efd42d40e1f0f6
shreya-sridhar <shreyasridhar1109@gmail.com>
2020-12-17 16:42:32 -0800

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
+
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 60 }
+    validates :summary, length: { maximum: 50 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
+
+#   is invalid with too short content (FAILED - 2)
+#   is invalid with a long summary (FAILED - 3)
+#   is invalid with a category outside the choices (FAILED - 4)
+#   is invalid if not clickbait (FAILED - 5)
